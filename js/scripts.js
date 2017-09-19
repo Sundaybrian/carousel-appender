af2d74fc153cda6b3193640d30aa902601ac37a2
sunday brian <brians931@gmail.com>
2017-09-20 01:42:05 +0300

    Replace the active class for appending the first slide to the new name one

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-  $(".active").append('<img class="d-block img-fluid" src="img/5858.jpg" alt="first slide">');
+  $(".one").append('<img class="d-block img-fluid" src="img/5858.jpg" alt="first slide">');
   $(".mbili").append('<img class="d-block img-fluid" src="img/8526.jpg" alt="second slide">');
   $(".tatu").append('<img class="d-block img-fluid" src="img/9083.jpg" alt="second slide">');
   $(".nne").append('<img class="d-block img-fluid" src="img/9316.jpg" alt="second slide">');
